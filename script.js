49985ef11c160b68e56dcac864743d185eec8c56
Lotzi <itskojabar@gmail.com>
2026-08-21 18:33:23 -0400

    Update image paths in truffledTable

--- script.js
+++ script.js
@@ -85,15 +85,15 @@
         { url: "https://extrememath.cyou", img: "/images/extrememathtextlogo.png", final: "/uv.html?site=" }
     ];
     const truffledTable = [
-        { url: "https://truffled.lol/", img: "/favicon.ico", final: "" },
-        { url: "https://boon.busse.li/", img: "/favicon.ico", final: "" },
-        { url: "https://buff.loscantarostemuco.cl/", img: "/favicon.ico", final: "" },
-        { url: "https://hibrooklyn.site/", img: "/favicon.ico", final: "" },
-        { url: "https://for-ravipati03121e3.shared-with.de/", img: "/favicon.ico", final: "" },
-        { url: "https://tutoring-services.org", img: "/favicon.ico", final: "" },
-        { url: "https://mathteachersforhire.org", img: "/favicon.ico", final: "" },
-        { url: "https://classlink.com.de", img: "/favicon.ico", final: "" },
-        { url: "https://geometrycalculatorhelprvhs.college", img: "/favicon.ico", final: "" }
+        { url: "https://truffled.lol", img: "/png/logo.png", final: "" },
+        { url: "https://boon.busse.li", img: "/png/logo.png", final: "" },
+        { url: "https://buff.loscantarostemuco.cl", img: "/png/logo.png", final: "" },
+        { url: "https://hibrooklyn.site", img: "/png/logo.png", final: "" },
+        { url: "https://for-ravipati03121e3.shared-with.de", img: "/png/logo.png", final: "" },
+        { url: "https://tutoring-services.org", img: "/png/logo.png", final: "" },
+        { url: "https://mathteachersforhire.org", img: "/png/logo.png", final: "" },
+        { url: "https://classlink.com.de", img: "/png/logo.png", final: "" },
+        { url: "https://geometrycalculatorhelprvhs.college", img: "/png/logo.png", final: "" }
     ];
     const frogieeTable = [
         { url: "https://frogiesarcade.win", img: "/stuff/logo.png", final: "" },
