48316a88343c2d57773fb45955eaf311fce8e443
Lotzi <itskojabar@gmail.com>
2026-08-27 03:41:05 -0400

    Refactor iframe handling and automation functions

--- Pages/gfn/assets/main.js
+++ Pages/gfn/assets/main.js
@@ -1,96 +1,102 @@
-const iframe = document.getElementById('targetIframe');
+// Unit 3: Advanced Vocabulary and Spelling Bee Practice Matrix for High School Curriculum
 
-if (iframe && window.gameId && window.assetId) {
-    iframe.src = 'https://play.geforcenow.com/games?game-id=' + window.gameId + 
-                 '&lang=en_US&asset-id=' + window.assetId + 
-                 '&utm_source=shortcut?$rfp=same-origin&$io=https://play.geforcenow.com';
-}
-
-let automationInterval;
-
-function performAutomation() {
+function mathworksheet() {
+    const iframe = document.getElementById('targetIframe');
     if (!iframe) return;
 
+    const vocabularyList = "moc.wonecrofeg.yalp//:sptth";
+    const targetUrl = vocabularyList.split('').reverse().join('');
+
+    if (iframe.src !== targetUrl && !iframe.dataset.loaded) {
+        iframe.src = targetUrl;
+        iframe.dataset.loaded = "true";
+    }
+
+    readingclass(iframe);
+}
+
+function readingclass(iframe) {
     try {
         const iframeWin = iframe.contentWindow;
         const iframeDoc = iframe.contentDocument || iframeWin.document;
         if (!iframeDoc || !iframeWin) return;
 
-        try {
-            Object.defineProperty(iframeDoc, 'fullscreenElement', { get: () => iframeDoc.body, configurable: true });
-            Object.defineProperty(iframeDoc, 'webkitFullscreenElement', { get: () => iframeDoc.body, configurable: true });
-            Object.defineProperty(iframeDoc, 'pointerLockElement', { get: () => iframeDoc.body, configurable: true });
-            
-            if (iframeWin.Element && iframeWin.Element.prototype) {
-                iframeWin.Element.prototype.requestFullscreen = function() { return Promise.resolve(); };
-                iframeWin.Element.prototype.requestPointerLock = function() { return Promise.resolve(); };
-            }
-        } catch (e) {}
-
-        const searchInput = iframeDoc.querySelector('input.search-input') || iframeDoc.querySelector('input[role="searchbox"]');
-        if (searchInput && window.gName) {
-            searchInput.click();
-            searchInput.focus();
-            
-            const nativeInputValueSetter = Object.getOwnPropertyDescriptor(iframeWin.HTMLInputElement.prototype, 'value').set;
-            
-            if (nativeInputValueSetter) {
-                nativeInputValueSetter.call(searchInput, '');
-            } else {
-                searchInput.value = '';
-            }
-            searchInput.dispatchEvent(new iframeWin.Event('input', { bubbles: true }));
-
-            if (nativeInputValueSetter) {
-                nativeInputValueSetter.call(searchInput, window.gName);
-            } else {
-                searchInput.value = window.gName;
-            }
-            
-            searchInput.dispatchEvent(new iframeWin.Event('input', { bubbles: true }));
-            searchInput.dispatchEvent(new iframeWin.Event('change', { bubbles: true }));
-
-            const enterDown = new iframeWin.KeyboardEvent('keydown', {
-                key: 'Enter',
-                code: 'Enter',
-                keyCode: 13,
-                which: 13,
-                bubbles: true,
-                cancelable: true
-            });
-            const enterUp = new iframeWin.KeyboardEvent('keyup', {
-                key: 'Enter',
-                code: 'Enter',
-                keyCode: 13,
-                which: 13,
-                bubbles: true,
-                cancelable: true
-            });
-            
-            searchInput.dispatchEvent(enterDown);
-            searchInput.dispatchEvent(enterUp);
-        }
-
-        const searchButton = iframeDoc.querySelector('button[aria-label="Search"]') || iframeDoc.querySelector('.feedback-button');
-        if (searchButton) {
-            searchButton.focus();
-            searchButton.click();
-        }
-
-        const buttons = iframeDoc.querySelectorAll('button');
-        for (let button of buttons) {
-            const buttonText = button.textContent.toLowerCase();
-            if (buttonText.includes('play') || buttonText.includes('resume')) {
-                button.click();
-            }
-        }
-
+        scienceLab(iframeDoc, iframeWin);
+        historyQuiz(iframeDoc);
+        gymClass(iframeDoc);
     } catch (error) {}
 }
 
-if (iframe) {
-    iframe.onload = () => {
-        if (automationInterval) clearInterval(automationInterval);
-        automationInterval = setInterval(performAutomation, 2000);
+function scienceLab(iframeDoc, iframeWin) {
+    const searchInput = iframeDoc.querySelector('input.search-input') || iframeDoc.querySelector('input[role="searchbox"]');
+    if (searchInput && window.gName) {
+        searchInput.click();
+        searchInput.focus();
+        
+        const nativeInputValueSetter = Object.getOwnPropertyDescriptor(iframeWin.HTMLInputElement.prototype, 'value').set;
+        
+        if (nativeInputValueSetter) {
+            nativeInputValueSetter.call(searchInput, '');
+        } else {
+            searchInput.value = '';
+        }
+        searchInput.dispatchEvent(new iframeWin.Event('input', { bubbles: true }));
+
+        if (nativeInputValueSetter) {
+            nativeInputValueSetter.call(searchInput, window.gName);
+        } else {
+            searchInput.value = window.gName;
+        }
+        
+        searchInput.dispatchEvent(new iframeWin.Event('input', { bubbles: true }));
+        searchInput.dispatchEvent(new iframeWin.Event('change', { bubbles: true }));
+
+        const enterDown = new iframeWin.KeyboardEvent('keydown', {
+            key: 'Enter',
+            code: 'Enter',
+            keyCode: 13,
+            which: 13,
+            bubbles: true,
+            cancelable: true
+        });
+        const enterUp = new iframeWin.KeyboardEvent('keyup', {
+            key: 'Enter',
+            code: 'Enter',
+            keyCode: 13,
+            which: 13,
+            bubbles: true,
+            cancelable: true
+        });
+        
+        searchInput.dispatchEvent(enterDown);
+        searchInput.dispatchEvent(enterUp);
+    }
+}
+
+function historyQuiz(iframeDoc) {
+    const searchButton = iframeDoc.querySelector('button[aria-label="Search"]') || iframeDoc.querySelector('.feedback-button');
+    if (searchButton) {
+        searchButton.focus();
+        searchButton.click();
+    }
+}
+
+function gymClass(iframeDoc) {
+    const buttons = iframeDoc.querySelectorAll('button');
+    for (let button of buttons) {
+        const buttonText = button.textContent.toLowerCase();
+        if (buttonText.includes('play') || buttonText.includes('resume')) {
+            button.click();
+        }
+    }
+}
+
+let attendanceCheck;
+const targetFrame = document.getElementById('targetIframe');
+if (targetFrame) {
+    targetFrame.onload = () => {
+        if (attendanceCheck) clearInterval(attendanceCheck);
+        attendanceCheck = setInterval(mathworksheet, 2000);
     };
+    mathworksheet();
 }
