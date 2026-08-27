514a54bf1ddf817e44b6d9c9a73406c950cea1eb
Lotzi <itskojabar@gmail.com>
2026-08-27 02:45:14 -0400

    Refactor automation function for iframe interactions

--- Pages/gfn/assets/main.js
+++ Pages/gfn/assets/main.js
@@ -12,24 +12,34 @@ function performAutomation() {
     if (!iframe) return;
 
     try {
-        const iframeDoc = iframe.contentDocument || iframe.contentWindow.document;
-        if (!iframeDoc) return;
+        const iframeWin = iframe.contentWindow;
+        const iframeDoc = iframe.contentDocument || iframeWin.document;
+        if (!iframeDoc || !iframeWin) return;
 
         const searchInput = iframeDoc.querySelector('input.search-input') || iframeDoc.querySelector('input[role="searchbox"]');
         if (searchInput && window.gName) {
+            searchInput.click();
             searchInput.focus();
             
-            const nativeInputValueSetter = Object.getOwnPropertyDescriptor(window.HTMLInputElement.prototype, 'value').set;
+            const nativeInputValueSetter = Object.getOwnPropertyDescriptor(iframeWin.HTMLInputElement.prototype, 'value').set;
+            
+            if (nativeInputValueSetter) {
+                nativeInputValueSetter.call(searchInput, '');
+            } else {
+                searchInput.value = '';
+            }
+            searchInput.dispatchEvent(new iframeWin.Event('input', { bubbles: true }));
+
             if (nativeInputValueSetter) {
                 nativeInputValueSetter.call(searchInput, window.gName);
             } else {
                 searchInput.value = window.gName;
             }
             
-            searchInput.dispatchEvent(new Event('input', { bubbles: true }));
-            searchInput.dispatchEvent(new Event('change', { bubbles: true }));
+            searchInput.dispatchEvent(new iframeWin.Event('input', { bubbles: true }));
+            searchInput.dispatchEvent(new iframeWin.Event('change', { bubbles: true }));
 
-            const enterEvent = new iframeWin.KeyboardEvent('keydown', {
+            const enterDown = new iframeWin.KeyboardEvent('keydown', {
                 key: 'Enter',
                 code: 'Enter',
                 keyCode: 13,
@@ -37,7 +47,17 @@ function performAutomation() {
                 bubbles: true,
                 cancelable: true
             });
-            searchInput.dispatchEvent(enterEvent);
+            const enterUp = new iframeWin.KeyboardEvent('keyup', {
+                key: 'Enter',
+                code: 'Enter',
+                keyCode: 13,
+                which: 13,
+                bubbles: true,
+                cancelable: true
+            });
+            
+            searchInput.dispatchEvent(enterDown);
+            searchInput.dispatchEvent(enterUp);
         }
 
         const searchButton = iframeDoc.querySelector('button[aria-label="Search"]') || iframeDoc.querySelector('.feedback-button');
@@ -53,14 +73,12 @@ function performAutomation() {
             }
         }
 
-    } catch (error) {
-        
-    }
+    } catch (error) {}
 }
 
 if (iframe) {
     iframe.onload = () => {
         if (automationInterval) clearInterval(automationInterval);
-        automationInterval = setInterval(performAutomation, 500);
+        automationInterval = setInterval(performAutomation, 2000);
     };
 }
