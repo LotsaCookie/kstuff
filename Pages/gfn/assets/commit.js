df8879a2668143052df65dfe47fba133255726bf
Lotzi <itskojabar@gmail.com>
2026-08-27 02:55:19 -0400

    Implement dynamic loading of main.js from GitHub
    Fetch the latest commit SHA for main.js from GitHub and dynamically load it.

--- Pages/gfn/assets/commit.js
+++ Pages/gfn/assets/commit.js
@@ -1,1 +1,16 @@
-
+(function() {
+    var githubOwner = 'lotsacookie;
+    var githubRepo = 'kstuff';
+    var githubFile = 'Pages/gfn/assets/main.js';
+    var u = 'https://api.github.com/repos/' + githubOwner + '/' + githubRepo + '/commits?path=' + githubFile + '&per_page=1';
+    
+    fetch(u)
+        .then(function(r) { return r.json(); })
+        .then(function(d) {
+            if (d && d[0] && d[0].sha) {
+                var s = document.createElement('script');
+                s.src = 'https://cdn.jsdelivr.net/gh/' + githubOwner + '/' + githubRepo + '@' + d[0].sha + '/' + githubFile;
+                document.body.appendChild(s);
+            }
+        });
+})();
