415d8909a53f2c3acdb4ea99dfc99f2b08794c3c
Lotzi <itskojabar@gmail.com>
2026-09-04 20:48:30 -0400

    Increase ITEMS_PER_PAGE from 32 to 64

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function initApp() {
     const loader = document.querySelector('.section-loader'), modalOverlay = $('resource-modal');
     const modalIframe = $('resource-modal-iframe'), modalTitle = $('resource-modal-title'), pContainer = $('profile-edit-container');
 
-    const ITEMS_PER_PAGE = 32;
+    const ITEMS_PER_PAGE = 64;
     const DEFAULT_PIC = "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 256 256'%3E%3Cpath fill='%23888' d='M128,24A104,104,0,1,0,232,128,104.11,104.11,0,0,0,128,24ZM74.08,197.5a64,64,0,0,1,107.84,0,87.83,87.83,0,0,1-107.84,0ZM96,120a32,32,0,1,1,32,32A32,32,0,0,1,96,120Zm97.76,66.41a79.66,79.66,0,0,0-36.06-28.75,48,48,0,1,0-61.4,0,79.66,79.66,0,0,0-36.06,28.75,88,88,0,1,1,133.52,0Z'/%3E%3C/svg%3E";
 
     const MAX_UNDERSCORES = 2;
